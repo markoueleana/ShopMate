117239dba2093c1a06be8acc7d92b31baf6f2f7d
EleanaMark <markoueleana@gmail.com>
2025-04-27 01:31:03 +0300

    add cart endpoints and handlers

---
+++ src/Services/Cart/Cart.Api/Cart/DeleteCart/DeleteCartHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using BuildingBlocks.CQRS;
+using Cart.API.Entities;
+using Marten;
+
+namespace Cart.API.Cart.DeleteCart;
+
+public record DeleteCartCommand(string UserName) : ICommand<DeleteCartResult>;
+public record DeleteCartResult(bool IsSuccess);
+
+public class DeleteCartCommandHandler (IDocumentSession session) : ICommandHandler<DeleteCartCommand, DeleteCartResult>
+{
+    public async Task<DeleteCartResult> Handle(DeleteCartCommand commad, CancellationToken cancellationToken)
+    {
+        session.Delete<ShoppingCart>(commad.UserName);
+
+        await session.SaveChangesAsync(cancellationToken);
+        return new DeleteCartResult(true);
+    }
+}
--- src/Services/Cart/Cart.Api/Program.cs
+++ src/Services/Cart/Cart.Api/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Cart.API.Entities;
+using Carter;
+using Marten;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+
+
+var app = builder.Build();
+app.MapCarter();
+app.Run();
 
 app.Run();
