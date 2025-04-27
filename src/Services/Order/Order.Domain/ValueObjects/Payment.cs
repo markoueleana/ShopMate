6e2d00ec607ad558627efd5b523406c5dfa9c140
EleanaMark <markoueleana@gmail.com>
2025-04-27 15:11:43 +0300

    add expiration date in payment

--- src/Services/Order/Order.Domain/ValueObjects/Payment.cs
+++ src/Services/Order/Order.Domain/ValueObjects/Payment.cs
@@ -12,18 +12,20 @@ public record Payment
     public string CardNumber { get; set; } = default!;
     public string CVV { get; set; } = default!;
     public string PaymentMethod { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
 
     protected Payment() { }
-    private Payment(string cardName, string cardNumber, string cvv, string paymentMethod)
+    private Payment(string cardName, string cardNumber, string cvv, string paymentMethod, string expiration)
     {
         CardName = cardName;
         CardNumber = cardNumber;
         CVV = cvv; 
         PaymentMethod = paymentMethod;
+        Expiration = expiration;
     }
 
-    public static Payment CreatePayment(string cardName, string cardNumber, string cvv, string paymentMethod)
+    public static Payment CreatePayment(string cardName, string cardNumber, string cvv, string paymentMethod,string expiration)
     {
-        return new Payment(cardName, cardNumber, cvv, paymentMethod);
+        return new Payment(cardName, cardNumber, cvv, paymentMethod, expiration);
     }
 }
