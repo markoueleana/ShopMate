29cc6a61e59bdde1b4f5b4a486176b8e0328d262
EleanaMark <markoueleana@gmail.com>
2025-04-27 19:01:58 +0300

    fix for renaming folders

--- src/Services/Order/Ordering.API/Program.cs
+++ src/Services/Order/Ordering.API/Program.cs
@@ -1,7 +1,7 @@
 
 using Ordering.API;
 using Ordering.Application;
-using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
--- src/Services/Order/Ordering.Infrastructure/Data/DependencyInjection.cs
+++ src/Services/Order/Ordering.Infrastructure/Data/DependencyInjection.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Ordering.Infrastructure;
+namespace Ordering.Infrastructure.Data;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database")!;
+        services.AddDbContext<OrderDbContext>(opt => opt.UseSqlServer(connectionString));
         return services;
     }
 }
