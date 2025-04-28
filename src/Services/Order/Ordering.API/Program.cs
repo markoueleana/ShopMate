e6debdf9cc0e6905931bbec31dcdd5822e6d55bc
EleanaMark <markoueleana@gmail.com>
2025-04-28 16:28:29 +0300

    add DI for the consumer api

--- src/Services/Order/Ordering.API/Program.cs
+++ src/Services/Order/Ordering.API/Program.cs
@@ -7,7 +7,7 @@ using Ordering.Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
      .AddCarter();
 
