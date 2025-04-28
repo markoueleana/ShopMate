473abb384b0fd1fbd9b81cfa93fd720c0152b691
EleanaMark <markoueleana@gmail.com>
2025-04-28 12:52:29 +0300

    add checkout event

---
+++ src/BuildingBlocks/BuildingBlocksMessaging/Events/CheckoutCartEvent.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BuildingBlocksMessaging.Events;
+
+public record CheckoutCartEvent 
+{
+    public Guid CustomerId { get; private set; } = default!;
+    public string OrderName { get; private set; } = default!;
+    public string Country { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+    public string City { get; set; } = default!;
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string CVV { get; set; } = default!;
+    public string PaymentMethod { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+}
