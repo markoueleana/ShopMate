3febd3ca45a3111e4c100dfd7634f5cc685c07a2
EleanaMark <markoueleana@gmail.com>
2025-04-29 10:30:26 +0300

    add hangfire for creating batch jobs

--- src/Services/Order/Ordering.API/Program.cs
+++ src/Services/Order/Ordering.API/Program.cs
@@ -1,7 +1,11 @@
 
 using Carter;
+using Hangfire;
+using Hangfire.Annotations;
+using Hangfire.Dashboard;
 using Ordering.API;
 using Ordering.Application;
+using Ordering.Application.Orders;
 using Ordering.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,14 @@ builder.Services
     .AddInfrastructureServices(builder.Configuration)
      .AddCarter();
 
+builder.Services.AddHangfire(x =>
+    x.UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseInMemoryStorage()
+);
+
+builder.Services.AddHangfireServer();
+builder.Services.AddScoped<CustomerOrdersEmailJob>();
 var app = builder.Build();
 
 app.MapCarter();
@@ -18,4 +30,22 @@ if (app.Environment.IsDevelopment())
 {
     await app.InitializeDb();
 }
+
+var options = new DashboardOptions()
+{
+    Authorization = new[] { new MyAuthFilter() }
+};
+app.UseHangfireDashboard("/hangfire",options);
+
+RecurringJob.AddOrUpdate<CustomerOrdersEmailJob>("customer-orders-email-job", x => x.RunJob(),
+    Cron.Minutely); 
+
 app.Run();
+
+public class MyAuthFilter : IDashboardAuthorizationFilter
+{
+    public bool Authorize([NotNull] DashboardContext context)
+    {
+        return true;
+    }
+}
---
+++ src/Services/Order/Ordering.Application/Orders/CustomerOrdersEmailJob.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+
+namespace Ordering.Application.Orders;
+
+public class CustomerOrdersEmailJob 
+{
+    private IOrderDbContext orderDbContext;
+    public CustomerOrdersEmailJob(IOrderDbContext context)
+    {
+        orderDbContext = context;
+    }
+    public async Task RunJob() 
+    {
+        var customers = await orderDbContext.Customers
+                     .ToListAsync();
+
+        var toDate = DateTime.Now;
+        var fromDate = toDate.AddMonths(-1);
+
+        foreach (var customer in customers)
+        {
+            var orders = await orderDbContext.Orders
+                .AsNoTracking()
+                .Where(o => o.CustomerId == customer.Id)
+                .Where(o => o.CreatedAt >= fromDate && o.CreatedAt <= toDate)
+                .OrderBy(o => o.OrderName)
+                .ToListAsync();
+
+            //Simulating email
+            Console.WriteLine($"Dear Customer {customer.FirstName}, Your have ordered {orders.Count} in the previous month!");
+
+        }
+    }
+}
